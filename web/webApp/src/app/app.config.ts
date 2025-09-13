b9f7fe4cca5dfed17d20487b60380b68f1ea16ac
Vinicius Monaretto <vini.regimona@gmail.com>
2025-09-13 14:03:26 -0300

    fix frontend

--- web/webApp/src/app/app.config.ts
+++ web/webApp/src/app/app.config.ts
@@ -4,7 +4,13 @@ import { provideRouter } from '@angular/router';
 import { provideCharts } from 'ng2-charts';  // Correct function: provideCharts
 
 import { routes } from './app.routes';
+import { WS_URL_TOKEN } from '../services/server-conector.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideCharts()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideCharts(),
+    { provide: WS_URL_TOKEN, useValue: 'ws://localhost:8888/websocket' }
+  ]
 };
